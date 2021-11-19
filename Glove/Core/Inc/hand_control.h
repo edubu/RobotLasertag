675e33cd7337a953928af3463f08952d9bf86097
edubu <53409873+edubu@users.noreply.github.com>
2021-11-19 12:58:05 -0500

    handcontrol=

---
+++ Glove/Core/Inc/hand_control.h
@@ -0,0 +1,19 @@
+/*
+ * hand_control.h
+ *
+ *  Created on: Nov 19, 2021
+ *      Author: edubuq
+ */
+
+#ifndef INC_HAND_CONTROL_H_
+#define INC_HAND_CONTROL_H_
+
+
+#include "stm32f4xx_hal.h"
+
+uint8_t getMagnitude(int16_t x, int16_t y, int16_t z);
+
+int8_t * getDirection(int16_t x, int16_t y);
+
+
+#endif /* INC_HAND_CONTROL_H_ */
